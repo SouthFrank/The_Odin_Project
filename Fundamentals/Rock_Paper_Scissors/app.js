42e445f064c5ff3869a24d6aa8cd29537d80dde2
Frank South <frank@franksouth.dev>
2023-07-31 11:43:58 -0400

    Updated logic to include all possible scenarios, and added prompt within loop to allow user input on each round

--- Fundamentals/Rock_Paper_Scissors/app.js
+++ Fundamentals/Rock_Paper_Scissors/app.js
@@ -9,31 +9,44 @@ const getComputerChoice = () => {
     } else {
         return 'scissors';
     }
-}
+};
 
-const playerSelection = 'rock';
+// const computerSelection = getComputerChoice();
+
 let playerWinCount = 0;
 let compWinCount = 0;
 
 //Function that plays a round between the user and computer
 
-const playRound = (playerSelection, computerSelection) => {
-    if(playerSelection === 'rock'){
-        if(computerSelection === 'paper'){
-            console.log('You lose! Better luck next time.');
-        } else if(computerSelection === 'scissors'){
-            console.log('You\'ve done it! You won!');
-        } else {
-            console.log('Ugh, a tie.');
-        }
+// 
+
+const playRoundNew = (playerSelection, computerSelection) => {
+    if(playerSelection === computerSelection){
+        return 'Ugh, tied.';
+    } else if(playerSelection === 'rock' && computerSelection === 'scissors') {
+        playerWinCount++;
+        return 'You\'ve won this round!';
+    } else if(playerSelection === 'paper' && computerSelection === 'rock') {
+        playerWinCount++;
+        return 'You\'ve won this round!';
+    } else if(playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerWinCount++;
+        return 'You\'ve won this round!';
+    } else {
+        compWinCount++;
+        return 'You\'ve lost this round, better luck next time!';
     }
-};
+}
 
 const game = () => {
     for(let i = 0; i < 5; i++){
+        const playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase();
         const computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
+        console.log(playRoundNew(playerSelection, computerSelection));
+        console.log(playerWinCount);
     }
 };
 
 game();
+
+// console.log(playRound(playerSelection, computerSelection));
