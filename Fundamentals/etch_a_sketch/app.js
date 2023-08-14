960589343d4cbad0761f168b1b71b7f300aa488d
Frank South <frank@franksouth.dev>
2023-08-14 15:44:42 -0400

    Added a button that allows you to choose the grid size, and JS that dynamically changes the grid to fit within the container

--- Fundamentals/etch_a_sketch/app.js
+++ Fundamentals/etch_a_sketch/app.js
@@ -1,21 +1,39 @@
 
 const container = document.querySelector('.grid-container');
+const stylesheet = document.styleSheets[0];
+const gridBtn = document.querySelector('.grid-btn');
 
-const makeGrid = () => {
-    for(let i = 0; i < 256; i++){
+const makeGrid = (x) => {
+    const itemSize = (100/x);
+    console.log(itemSize);
+    for(let i = 0; i < x*x ; i++){
         const item = document.createElement('div');
         item.classList.add('grid-item');
+        stylesheet.cssRules[8].style.height = `calc(${itemSize}%)`;
+        console.log(stylesheet.cssRules[8].style.height);
+        stylesheet.cssRules[8].style.width = `calc(${itemSize}%)`;
+        console.log(stylesheet.cssRules[8].style.width);
         container.appendChild(item);
     }
 }
 
-makeGrid();
-
-const gridItems = document.querySelectorAll('.grid-item');
-
-gridItems.forEach(item => {
-    item.addEventListener('mouseover', () => {
-        item.classList.toggle('grid-color');
+// makeGrid(100);
+gridBtn.addEventListener('click', () => {
+    container.innerHTML = '';
+    const gridSize = parseInt(prompt('What size would you like the grid to be?'));
+    makeGrid(gridSize);
+    
+    const gridItems = document.querySelectorAll('.grid-item');
+    console.log(gridItems);
+    gridItems.forEach(item => {
+        item.addEventListener('mouseover', () => {
+            item.classList.toggle('grid-color');
+        })
     })
 })
+
+
+
+
+
 
