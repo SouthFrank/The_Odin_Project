718c1cae315690ac91d173017988e64c4322ce64
Frank South <frank@franksouth.dev>
2023-08-15 12:21:52 -0400

    Added 'click' event listener to all calculator buttons that uses a switch statement to update the 'output' accordingly

---
+++ Fundamentals/Calculator/app.js
@@ -0,0 +1,28 @@
+const output = document.querySelector('.output');
+
+let buttons = Array.from(document.querySelectorAll('button'));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'AC':
+                output.innerText = '';
+                break; 
+            case 'DEL':
+                if(output.innerText != 'undefined'){
+                    output.innerText = output.innerText.slice(0, -1);
+                break;
+                } 
+            case '=':
+                try{
+                    output.innerText = eval(output.innerText);
+                }
+                catch {
+                    output.innerText = 'Error!';
+                }
+                break;
+            default: 
+                output.innerText += e.target.innerText;
+        }
+    })
+})
