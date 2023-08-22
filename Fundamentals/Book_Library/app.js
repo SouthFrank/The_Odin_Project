0526d57d5a891a43436909637622292e4f02a6fc
Frank South <frank@franksouth.dev>
2023-08-22 13:11:02 -0400

    Created filler array, book constructor, loop to add books from the array, and event listener for the button that adds books

--- Fundamentals/Book_Library/app.js
+++ Fundamentals/Book_Library/app.js
@@ -1,15 +1,41 @@
-//constructor function to make books
+
+// function Book(title, author, pages, read) {
+//     this.title = title,
+//     this.author = author,
+//     this.pages = pages, 
+//     this.read = read,
+//     this.info = function() {
+//         return `The book ${title}, written by ${author}, contains ${pages} pages, and ${read}.`
+//     }
+// };
+
+// const theNameOfTheWind = new Book('The Name of the Wind', 'Patrick Rothfuss', 675, 'has been read');
+
+// console.log(theNameOfTheWind.info());    
+
+const myLibrary = ['The Hobbit', 'The Name of the Wind', 'The Crown Conspiracy', 'Legends & Lattes'];
+
+const bookshelf = document.querySelector('.bookshelf');
+const addBookBtn = document.querySelector('.add-book-btn')
 
 function Book(title, author, pages, read) {
-    this.title = title,
-    this.author = author,
-    this.pages = pages, 
-    this.read = read,
-    this.info = function() {
-        return `The book ${title}, written by ${author}, contains ${pages} pages, and ${read}.`
-    }
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
 };
 
-const theNameOfTheWind = new Book('The Name of the Wind', 'Patrick Rothfuss', 675, 'has been read');
+addBookBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log
+})
 
-console.log(theNameOfTheWind.info());    
+const addBookToLibrary = () => {
+    for(let i = 0; i < myLibrary.length; i++){
+        const newBook = document.createElement('div');
+        newBook.innerText = myLibrary[i];
+        bookshelf.appendChild(newBook);
+    }
+}
+
+addBookToLibrary();
