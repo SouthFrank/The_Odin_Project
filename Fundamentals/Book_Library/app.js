0fdfd0ce29af9d88eb6088bce83048516bed46f2
Frank South <frank@franksouth.dev>
2023-08-22 15:34:58 -0400

    Improved functions that add books to the myLibrary array and that present the array items as cards on the browser

--- Fundamentals/Book_Library/app.js
+++ Fundamentals/Book_Library/app.js
@@ -13,10 +13,12 @@
 
 // console.log(theNameOfTheWind.info());    
 
-const myLibrary = ['The Hobbit', 'The Name of the Wind', 'The Crown Conspiracy', 'Legends & Lattes'];
+const myLibrary = [];
+// 'The Hobbit', 'The Name of the Wind', 'The Crown Conspiracy', 'Legends & Lattes'
 
 const bookshelf = document.querySelector('.bookshelf');
-const addBookBtn = document.querySelector('.add-book-btn')
+const bookFormBtn = document.querySelector('.book-form-btn');
+const newBookBtn = document.querySelector('#new-book-btn');
 
 function Book(title, author, pages, read) {
     this.title = title
@@ -25,17 +27,44 @@ function Book(title, author, pages, read) {
     this.read = read
 };
 
-addBookBtn.addEventListener('click', (e) => {
+const addBookToLibrary = () => {
+        const libraryIndex = myLibrary[myLibrary.length - 1];
+        const newBook = document.createElement('div');
+        newBook.classList.add('book-item');
+        const bookTitle = document.createElement('h2');
+        const bookAuthor = document.createElement('h3');
+        const bookPages = document.createElement('h3');
+        const bookHaveRead = document.createElement('h3');
+        const bookDelete = document.createElement('div');
+        bookDelete.classList.add('book-delete');
+        bookTitle.innerText = libraryIndex.title;
+        bookAuthor.innerText = libraryIndex.author;
+        bookPages.innerText = libraryIndex.pages;
+        bookHaveRead.innerText = libraryIndex.read;
+        newBook.appendChild(bookTitle);   
+        newBook.appendChild(bookAuthor);   
+        newBook.appendChild(bookPages);   
+        newBook.appendChild(bookHaveRead);  
+        newBook.appendChild(bookDelete); 
+        bookshelf.appendChild(newBook);
+};
+
+bookFormBtn.addEventListener('click', (e) => {
     e.preventDefault();
     console.log
+});
+
+newBookBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const bookItem = new Book(e.target.form[0].value, e.target.form[1].value, e.target.form[2].value, e.target.form[3].value)
+    myLibrary.push(bookItem);
+    console.log(myLibrary[0].title);
+    addBookToLibrary();
 })
 
-const addBookToLibrary = () => {
-    for(let i = 0; i < myLibrary.length; i++){
-        const newBook = document.createElement('div');
-        newBook.innerText = myLibrary[i];
-        bookshelf.appendChild(newBook);
-    }
-}
 
-addBookToLibrary();
+
+// const holes = new Book('Holes', 'C.H. Hue', 375, true);
+// myLibrary.push(holes);
+
+// addBookToLibrary();
